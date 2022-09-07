8670a6c5756f9cf89429cd3bc5018330243e933f
Jessica Moraes <jm.jeska@gmail.com>
2022-09-07 13:37:43 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/jessica/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessica.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
